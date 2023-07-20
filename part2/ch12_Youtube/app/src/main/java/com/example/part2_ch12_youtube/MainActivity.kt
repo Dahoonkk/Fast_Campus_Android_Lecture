11a3198eef35be522dd18f04872b863c342405ba
Dahoonkk <iamkdh1@naver.com>
2023-07-20 21:01:56 +0900

    studying

--- part2/ch12_Youtube/app/src/main/java/com/example/part2_ch12_youtube/MainActivity.kt
+++ part2/ch12_Youtube/app/src/main/java/com/example/part2_ch12_youtube/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.part2_ch12_youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.part2_ch12_youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var videoAdapter: VideoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        videoAdapter = VideoAdapter(context = this)
+
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
+        val videoList = readData("videos.json", VideoList::class.java) ?: VideoList(emptyList())
+        videoAdapter.submitList(videoList.videos)
     }
 }
