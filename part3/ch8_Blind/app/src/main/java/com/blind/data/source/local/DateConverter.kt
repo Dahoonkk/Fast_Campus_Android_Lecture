105ac4285e4c6a28c9f8b8a336f94e115704e942
Dahoonkk <iamkdh1@naver.com>
2023-08-30 20:52:52 +0900

    start studying

---
+++ part3/ch8_Blind/app/src/main/java/com/blind/data/source/local/DateConverter.kt
@@ -0,0 +1,19 @@
+package com.blind.data.source.local
+
+import androidx.room.TypeConverter
+import com.blind.util.DateUtil
+import java.util.Date
+
+class DateConverter {
+    @TypeConverter
+    fun toDate(timestamp: String?) : Date? {
+        return timestamp?.let {
+            DateUtil.dbDateFormat.parse(it)
+        }
+    }
+
+    @TypeConverter
+    fun toTimeStamp(date: Date?) : String? {
+        return DateUtil.dbDateFormat.format(date)
+    }
+}
