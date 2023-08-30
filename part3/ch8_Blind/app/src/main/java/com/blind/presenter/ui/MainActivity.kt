66bb91e7248d62558c1390ceeb2b0c26c07068c7
Dahoonkk <iamkdh1@naver.com>
2023-08-30 21:15:55 +0900

    start studying

--- part3/ch8_Blind/app/src/main/java/com/blind/presenter/ui/MainActivity.kt
+++ part3/ch8_Blind/app/src/main/java/com/blind/presenter/ui/MainActivity.kt
@@ -2,12 +2,18 @@ package com.blind.presenter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.blind.R
 import com.blind.databinding.ActivityMainBinding
+import com.blind.domain.model.Content
+import com.blind.presenter.ui.list.ListAdapter
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter by lazy { ListAdapter(Handler()) }
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
@@ -18,5 +24,25 @@ class MainActivity : AppCompatActivity() {
 
     fun onClickAdd() {
 
+    }
+
+    inner class Handler {
+        fun onClickItem(item : Content) {
+
+        }
+
+        fun onLongClickItem(item: Content) : Boolean {
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("정말 삭제 하시겠습니까?")
+                .setPositiveButton("네") { _, _ ->
+
+                }
+                .setNegativeButton("아니오") { _, _ ->
+
+                }
+                .show()
+
+            return false
+        }
     }
 }
---
+++ part3/ch8_Blind/app/src/main/java/com/blind/presenter/ui/list/ListAdapter.kt
@@ -0,0 +1,36 @@
+package com.blind.presenter.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.blind.databinding.ItemContentBinding
+import com.blind.domain.model.Content
+import com.blind.presenter.ui.MainActivity
+import com.blind.presenter.ui.list.viewholder.ContentViewHolder
+
+class ListAdapter(private val handler: MainActivity.Handler) : ListAdapter<Content, ContentViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentViewHolder {
+        return ContentViewHolder(
+            ItemContentBinding.inflate(LayoutInflater.from(parent.context), parent, false), handler
+        )
+    }
+
+    override fun onBindViewHolder(holder: ContentViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Content>() {
+            override fun areItemsTheSame(oldItem: Content, newItem: Content): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Content, newItem: Content): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
