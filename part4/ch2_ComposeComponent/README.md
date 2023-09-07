a5e2d93ef2d5755e5a6a5c9afae379b85797651f
Dahoonkk <iamkdh1@naver.com>
2023-09-07 21:38:44 +0900

    start studying

---
+++ part4/ch2_ComposeComponent/README.md
@@ -0,0 +1,24 @@
+# 📢 Compose 콤포넌트
+
+---
+## 💪🏻 공부 할 콤포넌트 종류
+- Text
+- Button
+- Modifier
+- Surface
+- Box
+- Row
+- Column
+- BoxWithConstraints
+- Image
+- Coli 라이브러리
+- Card
+- Checkbox
+- TextField
+- TopAppBar
+- Slot API
+- Scaffold
+- 카탈로그 앱 실습
+
+---
+### 
---
+++ part4/ch2_ComposeComponent/app/src/main/java/com/component/practice/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.component.practice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.component.practice.ui.theme.Part4_ch3_componentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Part4_ch3_componentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Part4_ch3_componentTheme {
+        Greeting("Android")
+    }
+}
